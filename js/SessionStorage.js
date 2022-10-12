fd14981a6536e17f8a58924351e0b12c7e5ab2c6
GMYA7 <105842196+GMYA7@users.noreply.github.com>
2022-10-11 22:35:09 -0600

    Update SessionStorage.js

--- js/SessionStorage.js
+++ js/SessionStorage.js
@@ -1,7 +1,7 @@
 function iniciar(){
     //Funcion para inicializar todos los eventos
     var btnTorneo = document.getElementById('registrar');
-    var btnLimpiar = document.getElementById('limpiar');
+    var btnLimpiar = document.getElementById('limpiar');    //Con estos botones lograremos recibir el evento del boton
     var btnClearLista = document.getElementById('clearList');
 
     if(btnTorneo.addEventListener){
@@ -60,7 +60,7 @@ function inscribir(){
         //CON .getItem logramos obtener lo almacenado en el navegador y con el JSON.PARSE lo
         // convertimos a un arreglo
         
-        //impresion de los datos
+        //impresion de los datos con el for que tiene como limite el numero de posiciones del arreglo
         for( var j = 0; j<4 ; j++){
             if(j == 0){
         var result = document.getElementById('detalles').innerHTML += "<br>";
